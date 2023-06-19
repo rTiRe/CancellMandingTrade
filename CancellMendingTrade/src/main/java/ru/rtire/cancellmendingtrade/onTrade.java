a4ab858d93758c5cc0cfe23498fa66b096cc58bf
Артём Тюрин <61313696+rTiRe@users.noreply.github.com>
2023-06-19 12:31:39 +0300

    Add files via upload

---
+++ CancellMendingTrade/src/main/java/ru/rtire/cancellmendingtrade/onTrade.java
@@ -0,0 +1,31 @@
+package ru.rtire.cancellmendingtrade;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Villager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+
+import java.util.Map;
+
+public class onTrade implements Listener {
+    @EventHandler
+    public void on(PlayerInteractAtEntityEvent e) {
+        if(e.getRightClicked().getType().equals(EntityType.VILLAGER)) {
+            Villager villager = (Villager) e.getRightClicked();
+            for(MerchantRecipe r : villager.getRecipes()) {
+                if(r.getResult().getItemMeta() instanceof EnchantmentStorageMeta) {
+                    EnchantmentStorageMeta meta = (EnchantmentStorageMeta) r.getResult().getItemMeta();
+                    for(Map.Entry<Enchantment, Integer> entry : meta.getStoredEnchants().entrySet()) {
+                        if(entry.getKey().equals(Enchantment.MENDING)) {
+                            r.setMaxUses(0);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
