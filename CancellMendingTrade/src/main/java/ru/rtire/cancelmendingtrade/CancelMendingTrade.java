4371ec18abb81ae2dbfc67c7ac9f5c0fd6e02757
Артём Тюрин <61313696+rTiRe@users.noreply.github.com>
2023-06-19 16:15:11 +0300

    Add files via upload

---
+++ CancellMendingTrade/src/main/java/ru/rtire/cancelmendingtrade/CancelMendingTrade.java
@@ -0,0 +1,33 @@
+package ru.rtire.cancelmendingtrade;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+
+public final class CancelMendingTrade extends JavaPlugin {
+
+    private static CancelMendingTrade instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        Bukkit.getPluginManager().registerEvents(new onTrade(), this);
+        System.out.println("from TiRe");
+
+        File config = new File(getDataFolder() + File.separator + "src/main/resources/config.yml");
+        if(!config.exists()) {
+            getConfig().options().copyDefaults(true);
+            saveDefaultConfig();
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    public static CancelMendingTrade getInstance() {
+        return instance;
+    }
+}
---
+++ CancellMendingTrade/src/main/java/ru/rtire/cancelmendingtrade/onTrade.java
@@ -0,0 +1,70 @@
+package ru.rtire.cancelmendingtrade;
+
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.WanderingTrader;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.MerchantRecipe;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class onTrade implements Listener {
+
+    private static CancelMendingTrade plugin;
+
+    public onTrade() { plugin = CancelMendingTrade.getInstance(); }
+
+    @EventHandler (priority = EventPriority.HIGHEST, ignoreCancelled = false)
+    public void on(PlayerInteractAtEntityEvent e) {
+        if(e.getRightClicked().getType().equals(EntityType.VILLAGER)) {
+            Villager villager = (Villager) e.getRightClicked();
+            for(MerchantRecipe r : villager.getRecipes()) {
+                if(r.getResult().getItemMeta() instanceof EnchantmentStorageMeta) {
+                    EnchantmentStorageMeta meta = (EnchantmentStorageMeta) r.getResult().getItemMeta();
+                    for(Map.Entry<Enchantment, Integer> entry : meta.getStoredEnchants().entrySet()) {
+                        if(entry.getKey().equals(Enchantment.MENDING)) {
+                            r.setMaxUses(0);
+                        }
+                    }
+                }
+            }
+        }
+        if(e.getRightClicked().getType().equals(EntityType.WANDERING_TRADER)) {
+            WanderingTrader trader = (WanderingTrader) e.getRightClicked();
+            List<MerchantRecipe> recipies = new ArrayList<>(trader.getRecipes());
+            for(MerchantRecipe r : recipies) {
+                if(r.getResult().getItemMeta() instanceof EnchantmentStorageMeta) {
+                    EnchantmentStorageMeta m = (EnchantmentStorageMeta) r.getResult().getItemMeta();
+                    for(Map.Entry<Enchantment, Integer> entry : m.getStoredEnchants().entrySet()) {
+                        if(entry.getKey().equals(Enchantment.MENDING)) {
+                            return;
+                        }
+                    }
+                }
+            }
+            ItemStack mending = new ItemStack(Material.ENCHANTED_BOOK);
+            EnchantmentStorageMeta meta = (EnchantmentStorageMeta) mending.getItemMeta();
+            assert meta != null;
+            meta.addStoredEnchant(Enchantment.MENDING, 1, false);
+            mending.setItemMeta(meta);
+            MerchantRecipe recipe = new MerchantRecipe(mending, plugin.getConfig().getInt("maximumPurchasesFromTheWanderingMerchant"));
+            recipe.addIngredient(new ItemStack(Material.EMERALD, random(plugin.getConfig().getInt("minPrice"), plugin.getConfig().getInt("maxPrice"))));
+            recipies.add(recipe);
+            trader.setRecipes(recipies);
+        }
+    }
+
+    private int random(int min, int max) {
+        max -= min;
+        return (int) (Math.random() * ++max) + min;
+    }
+}
